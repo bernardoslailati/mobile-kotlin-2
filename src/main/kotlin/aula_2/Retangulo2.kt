484b470f4518f2bab394347081d446dfb581b2fc
BernardoSlailati <44252209+BernardoSlailati@users.noreply.github.com>
2022-11-30 22:27:31 -0300

    edit aula2

--- src/main/kotlin/aula_2/Retangulo2.kt
+++ src/main/kotlin/aula_2/Retangulo2.kt
@@ -1,7 +1,16 @@
 package aula_2
 
-// Se inicializa com var comprimento = comprimento dá errado pq não verifica
-// Se faz pelo construtor primário e verifica em init não consegue ter set ou get
+// Exercício: Getters e Setters - feito por João Victor (para tirar dúvidas)
+// (*) Se inicializa com var comprimento = comprimento dá errado pq não verifica
+// (**) Se faz pelo construtor primário e verifica em init não consegue ter set ou get
+//
+// O que foi feito?
+//
+// Maneira de fazer com que o construtor receba PARÂMETROS a serem inseridos nos ATRIBUTOS da classe (comprimento,
+// linha 14 e largura, linha 23) passando pelas regras definidas nos SETS desses atributos.
+//
+// Além disso, os valores dos parâmetros do construtor são validados no bloco init ("Comprimento e largura não
+// podem ser negativos").
 
 class Retangulo2(comprimento: Double, largura: Double) {
 
