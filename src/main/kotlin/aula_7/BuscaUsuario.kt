ebd2d5c0f424980ad04e8d6a982c1d1f892d05c6
BernardoSlailati <44252209+BernardoSlailati@users.noreply.github.com>
2022-12-12 21:34:42 -0300

    add aula07

--- src/main/kotlin/aula_7/BuscaUsuario.kt
+++ src/main/kotlin/aula_7/BuscaUsuario.kt
@@ -5,10 +5,18 @@ import kotlin.random.Random
 
 data class Usuario(val nomeUsuario: String, val senha: String)
 
-sealed class ResultadoBuscaUsuario {
-    data class Sucesso(val usuarioBuscado: Usuario, val mensagem: String) : ResultadoBuscaUsuario()
-    object Carregando : ResultadoBuscaUsuario()
-    data class Falha(val erro: String) : ResultadoBuscaUsuario()
+sealed class ResultadoBuscaUsuario(val tipo: Int) {
+    data class Sucesso(val usuarioBuscado: Usuario, val mensagem: String) : ResultadoBuscaUsuario(tipo = 1)
+    object Carregando : ResultadoBuscaUsuario(tipo = 0) {
+        fun update(): ResultadoBuscaUsuario {
+            return Sucesso(usuarioBuscado = Usuario("", ""), mensagem = "Carregando foi atualizado!")
+        }
+    }
+    data class Falha(val erro: String) : ResultadoBuscaUsuario(tipo = -1)
+
+    fun buscaTipo() {
+        println("Meu tipo é $tipo.")
+    }
 }
 
 fun buscaUsuario(): ResultadoBuscaUsuario {
@@ -17,7 +25,7 @@ fun buscaUsuario(): ResultadoBuscaUsuario {
     return when(Random.nextInt(0, 3)) {
         0 -> Sucesso(usuarioBuscado = usuarioAleatorio, mensagem = "Sucesso ao buscar usuário!")
         1 -> Carregando
-        2 -> Falha(erro = "Falha ao buscar usuário")
+        2 -> Falha(erro = "Falha ao buscar usuário.")
         else -> Carregando
     }
 }
@@ -25,7 +33,14 @@ fun buscaUsuario(): ResultadoBuscaUsuario {
 fun main() {
 
     repeat(6) {
-        println(buscaUsuario())
+        when(val resposta = buscaUsuario()) {
+            is Sucesso -> println("${resposta.mensagem} O usuário buscado foi ${resposta.usuarioBuscado}.")
+            is Carregando -> {
+                    println("Carregando...")
+                    resposta.update()
+                }
+            is Falha -> println(resposta.erro)
+        }
     }
 
 }
